b919d817bd42dcbe627f160c8909b83349c390b1
Andre Faria <andre.faria.24@gmail.com>
2025-08-05 11:11:41 -0400

    remove ip_allowlist

--- main.tf
+++ main.tf
@@ -10,8 +10,4 @@ resource "hcp_vault_cluster" "main" {
   tier            = var.HCP_VAULT_CLUSTER_TIER
   proxy_endpoint  = var.HCP_VAULT_CLUSTER_PROXY_ENDPOINT
   public_endpoint = var.HCP_VAULT_CLUSTER_PUBLIC_ENDPOINT
-
-  ip_allowlist {
-    address = var.ALLOWED_IP
-  }
 }
