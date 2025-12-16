1dceccb39e228c32620670332c5041abd3aebe87
Andre Faria <andre.faria.24@gmail.com>
2025-12-16 09:58:49 -0500

    added additional outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "vault_cluster_uri" {
 output "vault_cluster_id" {
   value = hcp_vault_cluster.main.cluster_id
 }
+
+output "vault_namespace" {
+  value = hcp_vault_cluster.main.namespace
+}
+
+output "vault_region" {
+  value = hcp_vault_cluster.main.region
+}
+
+output "vault_version" {
+  value = hcp_vault_cluster.main.version
+}
+
+output "vault_org_id" {
+  value = hcp_vault_cluster.main.organization_id
+}
