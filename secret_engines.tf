71f565f441b1eccd12d437c6be53ca4dfd6f43d4
Andre Faria <andre.faria.24@gmail.com>
2025-12-16 11:51:06 -0500

    enable db & ldap secret engines

--- secret_engines.tf
+++ secret_engines.tf
@@ -8,3 +8,17 @@ resource "vault_mount" "kvv2_engine" {
     version = "2"
   }
 }
+
+# Enable LDAP secrets engine
+resource "vault_mount" "ldap" {
+  path        = "ldap"
+  type        = "ldap"
+  description = "LDAP secrets engine mount managed by Terraform"
+}
+
+# Enable Database secrets engine
+resource "vault_mount" "database" {
+  path        = "database"
+  type        = "database"
+  description = "Database secrets engine mount managed by Terraform"
+}
