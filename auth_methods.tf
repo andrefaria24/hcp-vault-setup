576ac8fd09fd36c7a23150015fda4560ff994e35
Andre Faria <andre.faria.24@gmail.com>
2025-12-17 09:46:48 -0500

    enalbe approle auth and create vault radar role/policy

--- auth_methods.tf
+++ auth_methods.tf
@@ -18,3 +18,22 @@ resource "vault_generic_endpoint" "acfaria_user" {
     policies = ["${resource.vault_policy.admin.name}"]
   })
 }
+
+# Enable AppRole authentication method
+resource "vault_auth_backend" "approle" {
+  type        = "approle"
+  path        = "approle"
+  description = "Enable AppRole authentication managed by Terraform"
+}
+
+# Vault Radar AppRole role
+resource "vault_approle_auth_backend_role" "vault_radar" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "vault-radar"
+  token_policies = [vault_policy.vault_radar.name]
+  token_ttl      = 28800 # 8 hours
+  token_max_ttl  = 86400 # 24 hours
+  bind_secret_id = true
+  token_num_uses = 0
+  depends_on     = [vault_auth_backend.approle]
+}
--- policies.tf
+++ policies.tf
@@ -1,9 +1,45 @@
-# Define an admin policy with full access
+# Admin policy with full access
 resource "vault_policy" "admin" {
   name   = "admin"
   policy = <<EOT
 path "*" {
   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
+
+# Vault Radar policy
+resource "vault_policy" "vault_radar" {
+  name   = "vault_radar"
+  policy = <<EOT
+path "kvv2/data/*" {
+  capabilities = ["create", "read", "update", "delete"]
+}
+
+path "kvv2/metadata/*" {
+  capabilities = ["create", "read", "update", "delete", "list"]
+}
+
+path "kvv2/metadata/" {
+  capabilities = ["list", "read"]
+}
+
+path "kvv2/undelete/*" {
+  capabilities = ["update"]
+}
+
+path "kvv2/destroy/*" {
+  capabilities = ["update"]
+}
+
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+path "auth/token/renew-self" {
+  capabilities = ["update"]
+}
+path "auth/token/revoke-self" {
+  capabilities = ["update"]
 }
 EOT
 }
