1a6506804be62d6c7adc6d8532131c1bb72d05d2
Andre Faria <andre.faria.24@gmail.com>
2025-12-18 09:23:45 -0500

    changed CA to lab.andrecfaria.com domain

--- pki.tf
+++ pki.tf
@@ -1,11 +1,11 @@
-# Create Root Certificate Authority for lab.local
+# Create Root Certificate Authority for lab.andrecfaria.com
 resource "vault_pki_secret_backend_root_cert" "root" {
   backend = vault_mount.pki.path
 
   type        = "internal"
-  common_name = "lab.local Root CA"
+  common_name = "lab.andrecfaria.com Root CA"
   ttl         = "87600h" # 10 years
-  issuer_name = "lab-local-root"
+  issuer_name = "lab-andrecfaria-com-root"
 
   key_type = "rsa"
   key_bits = 4096
@@ -16,24 +16,24 @@ resource "vault_pki_secret_backend_config_urls" "urls" {
   backend = vault_mount.pki.path
 
   issuing_certificates = [
-    "${hcp_vault_cluster.main.vault_public_endpoint_url}/v1/pki-lab-local/ca",
+    "${hcp_vault_cluster.main.vault_public_endpoint_url}/v1/pki-lab-andrecfaria-com/ca",
   ]
 
   crl_distribution_points = [
-    "${hcp_vault_cluster.main.vault_public_endpoint_url}/v1/pki-lab-local/crl",
+    "${hcp_vault_cluster.main.vault_public_endpoint_url}/v1/pki-lab-andrecfaria-com/crl",
   ]
 }
 
-# Role for lab.local issuance
-resource "vault_pki_secret_backend_role" "lab_local" {
+# Role for lab.andrecfaria.com issuance
+resource "vault_pki_secret_backend_role" "lab" {
   backend = vault_mount.pki.path
-  name    = "lab-local"
+  name    = "lab-andrecfaria-com"
 
   allow_localhost   = false
   enforce_hostnames = true
 
   # Permit lab.local + subdomains
-  allowed_domains  = ["lab.local"]
+  allowed_domains  = ["lab.andrecfaria.com"]
   allow_subdomains = true
 
   # Permit issuing for the bare domain as CN (optional)
